611ad32bfc7ea8339942c651d52659b295c8dafc
woosub-kim <woosubgo@gmail.com>
2022-04-27 11:50:08 +0900

    set default params for findexons

--- src/commons/Parameters.cpp
+++ src/commons/Parameters.cpp
@@ -2298,11 +2298,11 @@ void Parameters::setDefaults() {
     tarExclude = "^$";
     
     // findexons
-    orfBonusRatio = 0.1;
-    trimSpliceInScope = 90;
+    orfBonusRatio = 0.2;
+    trimSpliceInScope = 0;
     trimSpliceOutScope = 2;
     trimTermOutScope = 45;
-    trimTermInScope = 6;
+    trimTermInScope = 2;
 
     lcaRanks = "";
     showTaxLineage = 0;
